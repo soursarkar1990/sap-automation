5561851b48863f3501b98913c6388e246792335c
Sourav Sarkar <sourav.sarkar@asml.com>
2022-11-29 13:25:23 +0100

    indendation

--- deploy/terraform/run/sap_landscape/variables_local.tf
+++ deploy/terraform/run/sap_landscape/variables_local.tf
@@ -40,7 +40,7 @@ locals {
     tenant_id       = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value : null
   }
 
-cp_spn = {
+  cp_spn = {
     subscription_id = data.azurerm_key_vault_secret.cp_subscription_id.value,
     client_id       = var.use_spn ? data.azurerm_key_vault_secret.cp_client_id[0].value : null,
     client_secret   = var.use_spn ? data.azurerm_key_vault_secret.cp_client_secret[0].value : null,
