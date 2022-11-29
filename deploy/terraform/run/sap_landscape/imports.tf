4093f299f8fe52aec8ec2836d89056c677f5a5c2
Sourav Sarkar <sourav.sarkar@asml.com>
2022-11-29 17:35:01 +0100

    reverting back again

--- deploy/terraform/run/sap_landscape/imports.tf
+++ deploy/terraform/run/sap_landscape/imports.tf
@@ -15,8 +15,7 @@ data "terraform_remote_state" "deployer" {
     resource_group_name  = local.saplib_resource_group_name
     storage_account_name = local.tfstate_storage_account_name
     container_name       = local.tfstate_container_name
-    #key                  = var.deployer_tfstate_key
-    key                  = try(var.deployer_tfstate_key, "")
+    key                  = var.deployer_tfstate_key
     subscription_id      = local.saplib_subscription_id
   }
 }
